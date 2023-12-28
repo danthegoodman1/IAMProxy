33ba24a016216ce2b3a92a6f5a91a8cb1c4852ec
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-12-28 15:31:13 -0500

    add example control plane and demo client

---
+++ example_control_plane/index.ts
@@ -0,0 +1,22 @@
+import express, {Request} from 'express'
+
+const app = express()
+app.use(express.json())
+
+interface Key {
+    UserID: string
+    KeyID: string
+    SecretKey: string
+}
+
+app.post('/key/:keyID', async (req: Request<{keyID: string}, Key>, res) => {
+    res.json({
+        KeyID: "testuser",
+        SecretKey: "testpassword",
+        UserID: "testid"
+    })
+})
+
+app.listen('8888', () => {
+    console.log('listening on port 8888')
+})
